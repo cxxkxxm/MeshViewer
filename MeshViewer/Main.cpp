00c77835cb2bd9238a48f534f3c82eee096d3d91
Xu Chong <xc540863058@gmail.com>
2015-03-15 17:31:27 +0800

    start window

--- MeshViewer/Main.cpp
+++ MeshViewer/Main.cpp
@@ -1,8 +1,47 @@
 #include <iostream>
+
+#define GLEW_STATIC
+#include <GLEW\glew.h>
+
+#include <GLFW\glfw3.h>
+
 using namespace std;
 
+const GLuint WIDTH = 800, HEIGHT = 600;
+
 int main() {
-	cout << "Hello World!" << endl;
-	cin.get();
+	cout << "Starting GLFW context, OpenGL 3.3" << endl;
+
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "MeshViewer", nullptr, nullptr);
+	glfwMakeContextCurrent(window);
+	if (window == NULL)
+	{
+		cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glewExperimental = GL_TRUE;
+	if (glewInit() != GLEW_OK)
+	{
+		cout << "Failed to initialize GLEW" << std::endl;
+		return -1;
+	}
+
+	glViewport(0, 0, WIDTH, HEIGHT);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+		glfwSwapBuffers(window);
+	}
+
+	glfwTerminate();
 	return 0;
 }
